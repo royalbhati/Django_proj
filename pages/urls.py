c54811e26d51a214bfe4a1a48c39e88523ba85a2
Royal Bhati <royal240596@gmail.com>
2018-07-07 20:35:42 +0530

    second commit

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 
-urlpatterns=[ path('',views.index),
+urlpatterns=[ 
+			  path('',views.HomePage.as_view(),name='a'),
+			  path('about',views.About.as_view(),name='c'),	
 
 ]
 
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
-def index(request):
-	return HttpResponse('Hello')
+
+class HomePage(TemplateView):
+	template_name='pages/home.html'
+
+
+class About(TemplateView):
+	template_name='pages/about.html'
